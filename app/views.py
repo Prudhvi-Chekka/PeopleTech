d4a7ffb528337193a6ea20d45afd1bb1c12d7a45
Prudhvi-Chekka <80088878+Prudhvi-Chekka@users.noreply.github.com>
2021-03-25 09:17:48 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,8 +3,13 @@ from .models import Course,Student
 # Create your views here.
 
 def get_students(request):
-  return Student.objects.all().order_by('name')
+  return Student.objects.all().order_by('st_name')
 
 def get_courses(request):
   return Course.objects.all().order_by('crs_id')
-  
+
+def enroll(course):
+  pass
+
+def disenroll(course):
+  pass
