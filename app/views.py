777075452d4f1b268f6c0da99ae64f48b662cb60
Prudhvi-Chekka <80088878+Prudhvi-Chekka@users.noreply.github.com>
2021-03-25 09:09:33 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Course,Student
 # Create your views here.
+
+def get_students(request):
+  return Student.objects.all().order_by('name')
+
+def get_courses(request):
+  return Course.objects.all().order_by('crs_id')
+  
