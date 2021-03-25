03f258793cffb965496da7f0697f706f4f9c97c1
Prudhvi-Chekka <80088878+Prudhvi-Chekka@users.noreply.github.com>
2021-03-25 09:10:16 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Course(models.Model):
     #we will have an auto increment field in Django that is 'id',but we can initialise as well
     crs_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    crs_name = models.CharField(max_length=50)
 
 
 class Student(models.Model):
     st_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    st_name = models.CharField(max_length=50)
